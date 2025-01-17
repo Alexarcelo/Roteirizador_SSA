dcc45de8b510f5d9009eda74adafe06b3a7fda1f
Alexarcelo <marcelo@luckreceptivo.com.br>
2025-01-17 14:22:26 -0300

    Update Roteirizador_TRF_OUT.py

--- Roteirizador_TRF_OUT.py
+++ Roteirizador_TRF_OUT.py
@@ -10,6 +10,7 @@ import requests
 from google.cloud import secretmanager 
 import json
 from google.oauth2.service_account import Credentials
+from google.oauth2 import service_account
 
 def gerar_df_phoenix(vw_name, base_luck):
     
